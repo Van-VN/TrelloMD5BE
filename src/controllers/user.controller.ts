80339847747f531596e5433fa04581e728cc5f49
Van Vu <vanvn.vu@gmail.com>
2023-07-27 20:44:12 +0700

    fixes yarn

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import User from '../models/schemas/user.model';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import Post from '../models/schemas/post.model';
 
