2fa1ca715661cebdbc8c9c3b31026670f9558540
Van Vu <vanvn.vu@gmail.com>
2023-08-02 10:14:51 +0700

    Fixes create user logic

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -19,7 +19,12 @@ export default class UserController {
           password: hashedPassword,
           email: req.body.email
         });
-        return await user.save();
+        // return await user.save();
+        if (await user.save()) {
+          return res.json({ message: 'Tạo thành công người dùng' });
+        } else {
+          return res.json({ message: 'Có lỗi xảy ra, vui lòng thử lại!' });
+        }
       }
     } catch (err) {
       console.log(err);
