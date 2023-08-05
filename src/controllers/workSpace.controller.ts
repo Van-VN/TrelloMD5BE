554a92ddc9793afb030b9911c2c72380d51bb75c
Van-VN <vanvn.vu@gmail.com>
2023-08-05 15:33:21 +0700

    Added backend code for adding user to WS

--- src/controllers/workSpace.controller.ts
+++ src/controllers/workSpace.controller.ts
@@ -1,18 +1,25 @@
+import User from '../models/schemas/user.model';
 import WorkSpace from '../models/schemas/workspace.model';
 // import { Schema } from 'mongoose';
 
 export default class WorkSpaceController {
   static async listWorkspaces(req: any, res: any) {
     try {
-      return res.json({ workspaces: await WorkSpace.find({'users.idUser': req.params.userId}) })
+      return res.json({
+        workspaces: await WorkSpace.find({ 'users.idUser': req.params.userId })
+      });
     } catch (err) {
       console.log(err);
     }
   }
   static async getWorkspace(req: any, res: any) {
     try {
-      return res.json({ workSpace: await WorkSpace.findOne({ _id: req.params.id }).populate('users.idUser') });
-    } catch (e){
+      return res.json({
+        workSpace: await WorkSpace.findOne({ _id: req.params.id }).populate(
+          'users.idUser'
+        )
+      });
+    } catch (e) {
       console.log(e);
     }
   }
@@ -21,10 +28,12 @@ export default class WorkSpaceController {
       const workSpace = new WorkSpace({
         name: req.body.name,
         bio: req.body.bio,
-        users: [{
-          role: "admin",
-          idUser: req.body.userId
-        }],
+        users: [
+          {
+            role: 'admin',
+            idUser: req.body.userId
+          }
+        ]
       });
       await workSpace.save();
       return res.json({ message: 'Tạo workspace thành công' });
@@ -34,4 +43,35 @@ export default class WorkSpaceController {
     }
   }
 
+  static async addUserToWorkspace(req: any, res: any) {
+    try {
+      const userId = req.body.userId;
+      const wsId = req.body.wsId;
+      const user = await User.findOne({ _id: userId });
+      const workspace = await WorkSpace.findOne({ _id: wsId });
+      if (workspace && user) {
+        const userDupplicateCheck = workspace.users.some((user) =>
+          user.idUser.equals(userId)
+        );
+        if (!userDupplicateCheck) {
+          await WorkSpace.updateOne(
+            { _id: wsId },
+            { $push: { users: { idUser: userId, role: 'member' } } }
+          );
+          return res.json({
+            message: `Thêm người dùng ${user.userName} vào workspace thành công!`
+          });
+        } else {
+          return res.json({
+            error: `Đã tồn tại người dùng ${user.userName} trong workspace`
+          });
+        }
+      } else {
+        return res.json({ error: 'Người dùng hoặc workspace không tồn tại' });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.json({ error: 'Có lỗi xảy ra, vui lòng thử lại sau!' });
+    }
+  }
 }
