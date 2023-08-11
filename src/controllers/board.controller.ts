55cb98cd69be7b93c0d43a167c316c8755225b8d
Van-VN <vanvn.vu@gmail.com>
2023-08-11 12:37:46 +0700

    added board detail

--- src/controllers/board.controller.ts
+++ src/controllers/board.controller.ts
@@ -1,47 +1,53 @@
-import Board from "../models/schemas/board.model";
-import User from "../models/schemas/user.model";
+import Board from '../models/schemas/board.model';
+import User from '../models/schemas/user.model';
 import WorkSpace from '../models/schemas/workspace.model';
 
 export default class BoardController {
   static async createBoard(req: any, res: any) {
     try {
-      const workspace = await WorkSpace.findOne({_id: req.body.workspace}).populate('boards.board')
+      const workspace = await WorkSpace.findOne({
+        _id: req.body.workspace
+      }).populate('boards.board');
 
       // @ts-ignore
-      const titleCheck = workspace.boards.some(boardObj => boardObj.board.title === req.body.title);
+      const titleCheck = workspace.boards.some(
+        // @ts-ignore
+        (boardObj) => boardObj.board.title === req.body.title
+      );
       console.log(titleCheck);
       if (titleCheck) {
         return res.json({
           errorMessage: 'Board already exists'
-        })
+        });
       } else {
-        const user = await User.findOne({_id: req.body.userID})
+        const user = await User.findOne({ _id: req.body.userID });
         const board = new Board({
           title: req.body.title,
           backgroundImage: req.body.backgroundImage,
-          users: [{
-            role: "admin",
-            idUser: user
-          }]
+          users: [
+            {
+              role: 'admin',
+              idUser: user
+            }
+          ]
         });
         if (await board.save()) {
           workspace.boards.push({
-            board: board._id,
-          })
-          workspace.save()
+            board: board._id
+          });
+          workspace.save();
           return res.json({
             message: `create board successfully`,
             board: board,
             workspaceId: workspace._id
-          })
+          });
         }
-
       }
     } catch (error) {
       console.log(error);
       return res.json({
         errorMessage: `Something went wrong`
-      })
+      });
     }
   }
   static async addMember(req: any, res: any) {
@@ -53,7 +59,9 @@ export default class BoardController {
         message: `User ${req.params.nameMember} does not exist`
       });
     } else {
-      const isIdUser = boardCheck.users.some(user => user.idUser.toString() === userCheck._id.toString());
+      const isIdUser = boardCheck.users.some(
+        (user) => user.idUser.toString() === userCheck._id.toString()
+      );
       if (isIdUser) {
         return res.json({
           message: `User ${req.params.nameMember} already on board`
@@ -61,18 +69,26 @@ export default class BoardController {
       } else {
         boardCheck.users.push({
           idUser: userCheck._id,
-          role: ""
-        })
+          role: ''
+        });
         await Board.updateOne(
           { _id: req.params.idBoard },
           { $set: { users: boardCheck.users } }
-        )
+        );
 
         return res.json({
           message: `Add member ${req.params.nameMember} successfully`
         });
       }
+    }
+  }
 
+  static async getBoardDetail(req: any, res: any) {
+    try {
+      const board = await Board.findOne({ _id: req.params.id });
+      res.json({ board: board });
+    } catch (err) {
+      console.log(err);
     }
   }
 }
